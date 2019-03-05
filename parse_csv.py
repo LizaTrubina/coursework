c434e395e6c9fd40d50e81bfdf968551d69bfa66
componavt <andrew.krizhanovsky@gmail.com>
2019-03-05 13:18:04 +0300

    check year and rating presence

--- parse_csv.py
+++ parse_csv.py
@@ -28,7 +28,7 @@ for line in f:
     # extract name of person, print Name="..", 
     # split by comma, extract (1) expert rating, (2) year of article creation, 
     # e.g. rating=N, article_year=2013
-    print("source = "+line)
+    # print("source = "+line)
     
     fio= getSurnameAndName(line)
     # print("fio: {}\n".format(fio))
@@ -38,15 +38,19 @@ for line in f:
     line = line[end + 2:]
     line = line.split(',')
     # print("after split line = {}".format(line))
-    d = line[1]
-    #  d = int(line[1])
+    rat = line[1]
+    #  rat = int(line[1])
     year = line[5]
+    # print( 'year : ' +"{}\n".format(year))  
+
+    if year and rat:            # parse only the year is available
+        date[fio] = year
+        rating[fio] = rat
+        print('rating:' + fio + ': ' + rating[fio])
+        print( fio + ': ' +"{}\n".format(date[fio]))  
+
     # year = int(line[5])
     # i = int(year) - 2001
     
-    rating[fio] = d
-    date[fio] = year
-    print('rating:' + fio + ': ' + rating[fio])
-    print( fio + ': ' +"{}\n".format(date[fio]))  
 
 f.close() 
