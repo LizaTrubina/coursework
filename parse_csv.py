850440b605e2f7e40bcd514b22e4afc8f7841f1f
Liza <eliza.trubina@gmail.com>
2019-04-08 22:18:31 +0300

    Update parse_csv.py

--- parse_csv.py
+++ parse_csv.py
@@ -11,17 +11,20 @@ def getSurnameAndName(string):
     return name
     
 import os
+import codecs
 
 # read from from folder ./data
-filename = os.path.join(os.path.dirname(__file__), 'data', 'WP_Person_math.csv')
+filename = os.path.join(os.path.dirname(__file__), 'data', '19DecWP_Person_math_matem.csv')
+# filename = os.path.join(os.path.dirname(__file__), 'data', 'WP_Person_math.csv')
 
-f = open( filename, 'r')
-date = dict()
-rating = dict()
+f = codecs.open( filename, "r", "utf-8" )
+# f = open( filename, 'r')
+# date = dict()
+# rating = dict()
 my_rating = dict()
 
 for line in f:
-    line.encode('cp1251').decode('utf-8')
+ #   line.encode('cp1251').decode('utf-8')
     # extract name of person, print Name="..", 
     # split by comma, extract (1) expert rating, (2) year of article creation, 
     # e.g. rating=N, article_year=2013
@@ -36,11 +39,11 @@ for line in f:
     year = line[5]
 
     if year and rat:            # parse only the year is available
-        date[fio] = year
+       # date[fio] = year
         i = int(year)-2001
-        rating[fio] = rat
+       # rating[fio] = rat
         j = int(rat)
-        sum= round((0.46 * j)+ (0.3* i),0)
+        sum= round((0.38 * i)+ (0.3* j),1)
         my_rating[fio] = sum
         print( fio + ': ' +"{}\n".format(my_rating[fio])) 
        # print('rating:' + fio + ': ' + rating[fio])
