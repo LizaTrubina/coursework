6b188c78779e6f3ec17a751355ed1b45573ffd91
Liza <eliza.trubina@gmail.com>
2019-04-19 09:19:59 +0300

    Update parse_csv.py

--- parse_csv.py
+++ parse_csv.py
@@ -15,12 +15,10 @@ import codecs
 
 # read from from folder ./data
 filename = os.path.join(os.path.dirname(__file__), 'data', '19DecWP_Person_math_matem.csv')
-# filename = os.path.join(os.path.dirname(__file__), 'data', 'WP_Person_math.csv')
 
 f = codecs.open( filename, "r", "utf-8" )
 # f = open( filename, 'r')
-# date = dict()
-# rating = dict()
+
 my_rating = dict()
 
 for line in f:
@@ -34,21 +32,20 @@ for line in f:
     # cut off the surname and name from line
     end = line.rindex('"',0, len(line))
     line = line[end + 2:]
-    print("line = '{0}'".format(line))
     line = line.split(',')
-    print("len(line) = {0}".format(len(line)))
-    rat = line[1]
-    year = line[5]
+    # print("len(line) = {0}".format(len(line)))
+    if len(line) == 33 and line[1] and line[5]:
 
-    if year and rat:            # parse only the year is available
-       # date[fio] = year
+        rating = line[1]
+        year = line[5]
+        # if year and rating:            # parse only the year is available
         age = 2019-int(year)
-       # rating[fio] = rat
-        sum= round((0.38 * age)+ (0.3* int(rat)),1) # todo move round to print format
+        sum= round((0.38 * age)+ (0.3* int(rating)),0) # todo move round to print format
         my_rating[fio] = sum
-        print( fio + ': ' +"{}\n".format(my_rating[fio])) 
-       # print('rating:' + fio + ': ' + rating[fio])
-       # print( fio + ': ' +"{}\n".format(date[fio]))  
- 
 
+        print( fio + ': ' +"{}\n".format(my_rating[fio])) 
+                    # print('rating:' + fio + ': ' + rating[fio])
+                   # print( fio + ': ' +"{}\n".format(date[fio]))
+    else: continue
+    
 f.close() 
