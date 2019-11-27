c7e1b9ffd96cfe48929db3797db3110ec4b9a93d
componavt <andrew.krizhanovsky@gmail.com>
2019-11-27 13:16:27 +0300

    string to int

--- 1.py
+++ 1.py
@@ -6,14 +6,14 @@ if __name__ == '__main__':
 
    # Значения по X
    data_x = []
-   with open("1x.txt") as f:
+   with open("x.txt") as f:
       for line in f:
-         data_x.append(line)
+         data_x.append( int(line))
    # Значения по Y
    data_y = []
-   with open("1y.txt") as f:
+   with open("y.txt") as f:
       for line in f:
-         data_y.append(line)
+         data_y.append( int(line))
          
    # plt.axis([2000, 2020, 0, 10])
    plt.xlabel("X")
@@ -21,7 +21,16 @@ if __name__ == '__main__':
    # представляем точки (х,у) кружочками диаметра 10
  #  plt.plot(data_x, data_y, 'r')
    plt.plot(data_x, data_y, 'ro-') 
+
+#   print("data_x") 
+#   print(*data_x) 
+#   print("data_y") 
+#   print(*data_y) 
+
+#   plt.plot([1, 2, 3, 4], [1, 4, 9, 16], 'ro')
+#   plt.axis([0, 6, 0, 20])
+
    # Сетка на фоне для улучшения восприятия
-   # plt.grid(True, linestyle='-', color='0.75')
+   plt.grid(True, linestyle='-', color='0.75')
  
    plt.show()
